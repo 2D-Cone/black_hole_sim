124455e5f5dc40467fc4d769d15df3e2dad9920f
Robert Stott <rlstott2@gmail.com>
2026-03-17 15:49:01 -0400

    default configs for adjusting sim functionality easily

--- config.py
+++ config.py
@@ -1,8 +1,14 @@
 #Consants
 G =  1.0 #6.67430×10−11 but normalize
 m = 1.0
-M = 1.0
-dt = 0.01
-horizon_r = 2.0 #for newtonian version, if particle gets too close to black hole it gets "captured"
+M = 100.0
+
+dt = 0.006
+horizon_r = 2.0 #for newtonian version, if particle gets too close to black hole it gets "captured" //schwarzschild-like radius
 num_steps = 20000
-r0 = 8.0 #initial orbit radius
+
+r0 = 6.0 #initial orbit radius
+
+DEFAULT_PRESET = "circular"
+DEFAULT_MODEL = "newtonian"
+DEFAULT_INTEGRATOR = "verlet"
